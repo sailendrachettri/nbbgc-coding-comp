690d87e84fd11ee66d77e0a4ff661fe0b23ae5dd
sailendrachettri <sailendra9083@gmail.com>
2023-06-28 13:08:21 +0530

    sum or two numbers without + operator

---
+++ basic3/find_prime_factor.cpp
@@ -0,0 +1,15 @@
+/* Date: 28 - 06 - 2023
+Author: Sailendra Chettri */
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+
+        return 0;
+}
