864bb52aa66f74558d99b6ad0daf50b6866ec2e0
sailendrachettri <sailendra9083@gmail.com>
2023-06-29 10:53:12 +0530

    calculate power with pow()

--- basic4/calculate_power.cpp
+++ basic4/calculate_power.cpp
@@ -5,13 +5,27 @@ Author: Sailendra Chettri */
 #include <bits/stdc++.h>
 using namespace std;
 
+int calculate(int, int);
+
 int main()
 {
     int base, power;
     cout << "Enter a base value and power value: ";
     cin >> base >> power;
 
-    cout << base << " rised to the " << power << " is " << pow(base, power) << endl;
+    cout << base << " rised to the " << power << " is " << calculate(base, power) << endl;
 
     return 0;
+}
+
+int calculate(int base, int power)
+{
+    int res = base;
+    for (int i = 1; i < power; i++)
+    {
+        // cout << "i: " << i << endl;
+        res = base * res;
+    }
+
+    return res;
 }
