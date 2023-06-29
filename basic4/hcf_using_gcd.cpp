64ee9edaf994a10452831da64e92dc41e579fb8f
sailendrachettri <sailendra9083@gmail.com>
2023-06-29 13:21:35 +0530

    hcf of two numbers

---
+++ basic4/hcf_using_gcd.cpp
@@ -0,0 +1,13 @@
+/* Date: 29 - 06 - 2023
+Author: Sailendra Chettri */
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "HCF: " << __gcd(60, 75) << endl;
+
+    return 0;
+}
